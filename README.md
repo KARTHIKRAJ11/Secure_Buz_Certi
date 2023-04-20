b4b03451b1febf03af66f26fb21b76c429e78296
GrandZENO11 <kingpeter1106@gmail.com>
2023-04-20 19:14:10 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# SecureBuzCertificate
+## SPIT Hackathon
+## PS9
+### T12 -Edgerunners
+- Paul Crescent
+- Karthik Raj
+- Justin Mathews
+
+## Installation
+
+Node.js App Installation Guide
+Prerequisites
+
+   - Node.js (version 10 or higher) installed on your system
+- npm (version 6 or higher) installed on your system
+
+### Step 1: Clone the Repository
+
+Use the following command to clone the repository to your local machine:
+
+`https://github.com/Pauloper1/SecureBuzCertificate`
+
+## Step 2: Install Dependencies
+
+Go to the project directory and install the required dependencies by running the following command:
+
+`npm install`
+
+## Step 3: Run the Application
+
+Use the following command to start the application:
+
+`npm start`
+
+The application should now be running on http://localhost:5000 in your web browser.
+## Troubleshooting
+
+If you encounter any issues during the installation process, please create an issue on the repository or send an email to the maintainer for support.
+
+---
+
+# Features
+This is a Platform for secure Business certificate applications
+
+It provides way to enable user to apply for them easily
+
+1. User need to go in there account and select the certificate they want to apply
+
+2. they simply need to read the requirements for the certificate
+
+3. upload the needed documents & submit
+
+4. Now wait for approval 
+
+--- 
+We also have other features such as 
+
++ language selection - to switch to their preferred language
+
++ chat bot
+
++ video verification
+    - some of the concerned staffs will verify your signature on a video meet.
+
---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require("express");
+// const bodyParser = require("body-parser");
+// const mongoose=require("mongoose");
+// const morgan=require("morgan");
+const bodyParser = require("body-parser");
+const mongoose=require("mongoose");
+// const morgan=require("morgan");
+
+const app = express()
+
+const dialogflow = require('@google-cloud/dialogflow').v2beta1;
+
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname + "/pages/HTML/signin.html");
+});
+
+app.get("/approve_certificate", (req,res)=>{
+    res.sendFile(__dirname + "/pages/HTML/user_certificate.html");
+});
+
+app.get("/underScrutiny", (req,res)=>{
+    res.sendFile(__dirname + "/pages/HTML/user_certificate_copy.html");
+});
+
+
+app.get("/adminDashboard",(req, res)=>{
+    res.sendFile(__dirname+"/pages/HTML/adminDashboard.html")
+})
+
+app.get("/signup.html", (req,res)=>{
+    res.sendFile(__dirname + "/pages/HTML/signup.html");
+});
+
+app.get("/Dashboard.html", (req,res)=>{
+    res.sendFile(__dirname + "/pages/HTML/Dashboard.html");
+});
+
+app.get("/pages/HTML/underScrutiny.html", (req,res)=>{
+    res.sendFile(__dirname + "/pages/HTML/underScrutiny.html")});
+app.get("/pages/Dashboard.html", (req,res)=>{
+    res.sendFile(__dirname + "/pages/HTML/Dashboard.html");
+});
+app.get("/pages/status.html", (req,res)=>{
+    res.sendFile(__dirname + "/pages/HTML/status.html");
+});
+
+
+app.get("/pages/needs.html", (req,res)=>{
+    res.sendFile(__dirname + "/pages/HTML/needs.html");
+});
+
+app.get("/pages/user_form.html", (req,res)=>{
+    res.sendFile(__dirname + "/pages/HTML/user_form.html");
+});
+
+
+app.post("/auth",(req,res)=>{
+    console.log(req.body)
+    if(req.body.email === BUser.email && req.body.passwd === BUser.passwd){
+        res.sendFile(__dirname + "/pages/HTML/dashboard.html")
+    }else{
+        res.send(`
+        <h1>wrong credentials</h1>
+        `)
+    }
+})
+
+
+app.listen(5000, ()=>{
+    console.log("Server started Running!");
+})
